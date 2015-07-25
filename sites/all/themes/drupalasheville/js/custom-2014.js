f36a8fdc484277d46fc4586c814111bba60a306a
Dustin Harrell <dustin@blueoi.com>
2015-07-25 18:41:25 -0400

    fixes for issue 40 ; need a photo to close issue 44

--- sites/all/themes/drupalasheville/js/custom-2014.js
+++ sites/all/themes/drupalasheville/js/custom-2014.js
@@ -1,44 +1,44 @@
-(function ($) {
-
-  var $masthead2014 = $('.mast-head-2014 .background'),
-      $mastheadtitle2014 = $('.mast-head-2014 .text'),
-      $logo2014 = $('.logo2014'),
-      $body = $(document),
-      $bodyHeight = $body.height();
-  
-  $(window).scroll(function () {
-  
-      var scrollTopValue = $body.scrollTop(),
-          scrollTopDivBodyHeight = scrollTopValue / $bodyHeight;
-          
-      if(scrollTopValue <= 0){
-        
-        var mastBlur = -(scrollTopValue * 0.3)+'px';
-        
-        var titleSize = 1 + -(scrollTopValue * 0.01);
-        
-        var logoSize = (223 - (scrollTopValue * 0.9));
-        var logoLeft = (20 + (scrollTopValue * 0.5));
-        
-        $masthead2014.css({
-          'background-size': (100 - (scrollTopValue * 0.5)) + '%',
-          '-webkit-filter': 'blur(' + mastBlur + ')',
-          '-moz-filter': 'blur(' + mastBlur + ')',
-          '-o-filter': 'blur(' + mastBlur + ')',
-          '-ms-filter': 'blur(' + mastBlur + ')',
-          'filter': 'blur(' + mastBlur + ')'
-        });
-        
-        $mastheadtitle2014.css({
-          'font-size': titleSize + 'em',
-        });
-        
-        $logo2014.css({
-          'background-size': logoSize + 'px',
-          'width': logoSize + 12 + 'px',
-          'height': logoSize + 12 + 'px',
-          'left': logoLeft + 'px'
-        }); 
-      }     
-  });
-})(jQuery);
+// (function ($) {
+//
+//   var $masthead2014 = $('.mast-head-2014 .background'),
+//       $mastheadtitle2014 = $('.mast-head-2014 .text'),
+//       $logo2014 = $('.logo2014'),
+//       $body = $(document),
+//       $bodyHeight = $body.height();
+//
+//   $(window).scroll(function () {
+//
+//       var scrollTopValue = $body.scrollTop(),
+//           scrollTopDivBodyHeight = scrollTopValue / $bodyHeight;
+//
+//       if(scrollTopValue <= 0){
+//
+//         var mastBlur = -(scrollTopValue * 0.3)+'px';
+//
+//         var titleSize = 1 + -(scrollTopValue * 0.01);
+//
+//         var logoSize = (223 - (scrollTopValue * 0.9));
+//         var logoLeft = (20 + (scrollTopValue * 0.5));
+//
+//         $masthead2014.css({
+//           'background-size': (100 - (scrollTopValue * 0.5)) + '%',
+//           '-webkit-filter': 'blur(' + mastBlur + ')',
+//           '-moz-filter': 'blur(' + mastBlur + ')',
+//           '-o-filter': 'blur(' + mastBlur + ')',
+//           '-ms-filter': 'blur(' + mastBlur + ')',
+//           'filter': 'blur(' + mastBlur + ')'
+//         });
+//
+//         $mastheadtitle2014.css({
+//           'font-size': titleSize + 'em',
+//         });
+//
+//         $logo2014.css({
+//           'background-size': logoSize + 'px',
+//           'width': logoSize + 12 + 'px',
+//           'height': logoSize + 12 + 'px',
+//           'left': logoLeft + 'px'
+//         });
+//       }
+//   });
+// })(jQuery);
